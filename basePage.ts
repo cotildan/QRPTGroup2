7c5e3228b67452b90eec767cf674d502fd0fde0b
Cotilda <cotilda@Cotildas-MacBook-Air.local>
2022-06-06 18:02:55 -0700

    basepage commit

---
+++ basePage.ts
@@ -0,0 +1,18 @@
+import{
+    Builder, By,
+    until, Capabilities,
+    WebDriver, WebElement
+} from "selenium-webdriver"
+import { elementLocated } from "selenium-webdriver/lib/until";
+
+export class basePage {
+    driver: WebDriver;
+    url: string = 'https://www.walmart.com/'
+    searchBar: By = By.name('q')
+
+
+    async navigate (){
+        await this.driver.get(this.url);
+        await this.driver.wait(elementLocated(this.searchBar))
+    }
+}
